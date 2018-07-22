47cb8720e20458a02812ea24f546cd5f75d086af
Jeremiah <jkleigh@gmail.com>
2018-07-22 14:19:54 -0500

    formatting update

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-//	fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+//  fmt.Printf("%d - %b - %x \n", 42, 42, 42)
 //  fmt.Printf("%d \t %b \t %#x \n", 42, 42, 42)
     for i := 0; i < 200; i++ {
       fmt.Printf("%d \t %b \t %x \t %q \n", i, i, i, i)
