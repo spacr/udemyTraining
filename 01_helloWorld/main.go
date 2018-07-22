193dff228d01474960c6acb5f432d5598a376dd6
Jeremiah <jkleigh@gmail.com>
2018-07-22 14:14:25 -0500

    updated hellow with loop

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+//	fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+//  fmt.Printf("%d \t %b \t %#x \n", 42, 42, 42)
+    for i := 0; i < 200; i++ {
+      fmt.Printf("%d \t %b \t %x \t %q \n", i, i, i, i)
+    }
 }
